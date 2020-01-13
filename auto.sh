0f2ac37a0d9194a469c2926efee3330fc8a18606
varshan <varshan@pop-os.localdomain>
2020-01-13 15:51:58 +0530

    Initial commit

---
+++ auto.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+conda deactivate
+heroku login
+git init
+heroku git:remote -a varshan
+git add .
+git commit -m "Initial commit"
+heroku addons:create heroku-postgresql:hobby-dev
+heroku config -s | grep DATABASE_URL
+heroku config:set DISABLE_COLLECTSTATIC=1
+pushing project to deploy
+git push heroku master
+heroku run python3 manage.py migrate
+heroku logs --tail
+
